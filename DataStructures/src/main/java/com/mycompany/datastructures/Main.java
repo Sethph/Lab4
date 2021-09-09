3e4e7a94b6e4ae7c90ce81219e45c3c5edea4531
Sethph <seth.hammond@go.tarleton.edu>
2021-09-09 12:04:50 -0500

    Intial Commit

---
+++ DataStructures/src/main/java/com/mycompany/datastructures/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.datastructures;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sethp
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue queue = new Queue(5);
+        int val = sc.nextInt();
+        while(val != 0){
+            if(queue.isFull()){
+                System.out.printf("Got: %d%n", queue.get());
+            }
+            queue.append(val);
+            val = sc.nextInt();
+        }
+        
+        while(!queue.ifEmpty()){
+            System.out.printf("Got: %d%n", queue.get());
+        }
+    }
+}
---
+++ DataStructures/src/main/java/com/mycompany/datastructures/Queue.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.datastructures;
+
+/**
+ *
+ * @author sethp
+ */
+public class Queue {
+    public int queue[];
+    public int index;
+    
+    public Queue(int size){
+        this.queue = new int[size];
+        index = 0;
+    }
+    
+    public void append(int value){
+        queue[index] = value;
+        index++;
+    }
+    
+    public int get(){
+        index--;
+        int getValue = queue[0];
+        System.arraycopy(queue, 1, queue, 0, index);
+        return getValue; 
+    }
+    
+    public boolean isFull(){
+        return index == queue.length;
+    }
+    
+    public boolean ifEmpty(){
+        return index == 0;
+    }
+    
+    
+}
